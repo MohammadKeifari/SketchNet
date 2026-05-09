e31dae040def0931583bb54a376968a78be23306
Mohammad Keifari <mohammadkeifari2007@gmail.com>
2026-05-09 23:35:56 +0330

    test_email_required

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -43,3 +43,10 @@ class CustomUserModelTests(TestCase):
             username="testuser", email="test@example.com", password="testpass123"
         )
         self.assertEqual(str(user), "testuser")
+
+    def test_email_required(self):
+        """Test email is required"""
+        with self.assertRaises(ValueError):
+            User.objects.create_user(
+                username="nouser", email="", password="testpass123"
+            )
