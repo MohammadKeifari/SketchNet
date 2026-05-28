72755bf28f3a5a4527fb23e0580d565fe4e040be
Mohammad Keifari <mohammadkeifari2007@gmail.com>
2026-05-28 13:56:17 +0330

    update_sidebar_preference

--- setting/views.py
+++ setting/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import JsonResponse
 
 
 @login_required
@@ -32,3 +33,23 @@ def settings_view(request):
             "settings": user_settings,
         },
     )
+
+
+@login_required
+def update_sidebar_preference(request):
+    """AJAX endpoint to update sidebar collapse preferences."""
+    if request.method == "POST":
+        import json
+
+        data = json.loads(request.body)
+        user_settings = request.user.settings
+
+        if "collapse_left_sidebar" in data:
+            user_settings.collapse_left_sidebar = data["collapse_left_sidebar"]
+        if "collapse_right_sidebar" in data:
+            user_settings.collapse_right_sidebar = data["collapse_right_sidebar"]
+
+        user_settings.save()
+        return JsonResponse({"success": True})
+
+    return JsonResponse({"error": "Invalid request"}, status=400)
