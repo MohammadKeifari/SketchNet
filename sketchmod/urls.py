6138941ac9aca94814384ba31daa1534de1e79fd
Mohammad Keifari <mohammadkeifari2007@gmail.com>
2026-05-29 19:24:35 +0330

    graph validation api

--- sketchmod/urls.py
+++ sketchmod/urls.py
@@ -6,6 +6,7 @@ app_name = "sketchmod"
 urlpatterns = [
     path("", views.canvas, name="canvas"),
     path("api/export/", views.export_api, name="export_api"),
+    path("api/validate/", views.validate_api, name="validate_api"),
     path(
         "api/<str:dataset_id>/columns/",
         views.api_dataset_columns,
