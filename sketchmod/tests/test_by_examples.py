5278c2ca657c6b98f828cea614aa2feb69a019b9
Mohammad Keifari <mohammadkeifari2007@gmail.com>
2026-05-30 20:21:02 +0330

    test: update test_by_examples.py fixing bug

--- sketchmod/tests/test_by_examples.py
+++ sketchmod/tests/test_by_examples.py
@@ -359,7 +359,7 @@ class ExampleGraphTests(SimpleTestCase):
         self.assertIn("def load_data():", code)
         self.assertIn("def train_model(", code)
         self.assertIn("def visualize(", code)
-        self.assertIn("Standard", code)
+        self.assertIn("Z-score", code)
         self.assertIn("Early stopping", code)
         self.assertIn("torch.nn.utils.clip_grad_norm_", code)
         compile(code, "<test>", "exec")
