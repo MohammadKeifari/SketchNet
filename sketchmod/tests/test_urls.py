f34ab223d4554bdfb55268384382de80ff1990cd
Mohammad Keifari <mohammadkeifari2007@gmail.com>
2026-06-03 20:22:34 +0330

    test: create test_urls.py

---
+++ sketchmod/tests/test_urls.py
@@ -0,0 +1,26 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from sketchmod import views
+
+
+class UrlsTest(SimpleTestCase):
+    def test_canvas_url_resolves(self):
+        url = reverse("sketchmod:canvas")
+        self.assertEqual(resolve(url).func, views.canvas)
+
+    def test_export_api_url_resolves(self):
+        url = reverse("sketchmod:export_api")
+        self.assertEqual(resolve(url).func, views.export_api)
+
+    def test_validate_api_url_resolves(self):
+        url = reverse("sketchmod:validate_api")
+        self.assertEqual(resolve(url).func, views.validate_api)
+
+    def test_highlight_path_api_url_resolves(self):
+        url = reverse("sketchmod:highlight_path_api")
+        self.assertEqual(resolve(url).func, views.highlight_path_api)
+
+    def test_dataset_columns_url_resolves(self):
+        # This URL expects a dataset_id parameter
+        url = reverse("sketchmod:api_dataset_columns", args=["abc12345"])
+        self.assertEqual(resolve(url).func, views.api_dataset_columns)
