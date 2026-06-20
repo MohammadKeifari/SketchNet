fa79a807177496b9d7dbb83551a9b946c9627ef2
Mohammad Keifari <mohammadkeifari2007@gmail.com>
2026-06-20 22:56:51 +0330

    new load template urls

--- sketchmod/urls.py
+++ sketchmod/urls.py
@@ -7,10 +7,14 @@ urlpatterns = [
     path("", views.canvas, name="canvas"),
     path("api/export/", views.export_api, name="export_api"),
     path("api/validate/", views.validate_api, name="validate_api"),
+    path("api/highlight-path/", views.highlight_path_api, name="highlight_path_api"),
     path(
         "api/<str:dataset_id>/columns/",
         views.api_dataset_columns,
         name="api_dataset_columns",
     ),
-    path("api/highlight-path/", views.highlight_path_api, name="highlight_path_api"),
+    path(
+        "api/consume-template/", views.consume_template_api, name="consume_template_api"
+    ),
+    path("template/<str:template_name>/", views.template_view, name="template_load"),
 ]
